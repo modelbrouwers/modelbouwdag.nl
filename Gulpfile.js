73a2e428e2df529f43011d9460a2715b3ca790e3
Sergei Maertens <sergei@maykinmedia.nl>
2016-04-14 15:28:49 +0200

    Fix some styling issues for modelbouwdag

--- Gulpfile.js
+++ Gulpfile.js
@@ -27,7 +27,9 @@ gulp.task('sass', function() {
 });
 
 
-// watch task
-gulp.task('default',function() {
+gulp.task('watch', function() {
     gulp.watch(sass_src, ['sass']);
-});
+})
+
+
+gulp.task('default', ['sass', 'watch']);
